4e11580de9add43a481869eee7bd9f83c356ad56
Ouail-01 <ouaillaarabi@gmail.com>
2023-07-04 13:10:29 +0100

    Update item file

--- item.rb
+++ item.rb
@@ -1,7 +1,8 @@
 class Item
-  attr_accessor :id
+  attr_accessor :publish_date, :archived
+  attr_reader :id, :genre, :author, label
 
   def initialize()
-    @id = 1
+    @id = Random.rand(1..1000)
   end
 end
