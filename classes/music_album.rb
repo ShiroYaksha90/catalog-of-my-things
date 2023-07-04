1645a799322c52fd78ee80c43243e9df3a24d0a2
Basim Aljazaeri <30289772+ShiroYaksha90@users.noreply.github.com>
2023-07-04 15:53:36 +0300

    Fix linter

--- classes/music_album.rb
+++ classes/music_album.rb
@@ -1,20 +1,17 @@
-require_relative 'item.rb'
+require_relative 'item'
 
 class MusicAlbum < Item
-    attr_reader :id
-    attr_accessor :on_spotify
+  attr_reader :id
+  attr_accessor :on_spotify
 
-    def initialize(on_spotify, publish_date, archived: false)
-        super(id,publish_date,archived: archived)
-        @on_spotify = on_spotify
-    end
+  def initialize(on_spotify, publish_date, archived: false)
+    super(id, publish_date, archived: archived)
+    @on_spotify = on_spotify
+  end
 
-    private
+  private
 
-    def can_be_archived?(item)
-
-        super && on_spotify == true
-
-    end
-
+  def can_be_archived?(item)
+    super && on_spotify == true
+  end
 end
--- item.rb
+++
@@ -1,7 +0,0 @@
-class Item
-  attr_accessor :id
-
-  def initialize()
-    @id = 1
-  end
-end
