d8868ee7ee6deef4d6d2f1ce91f7f99e76ef3971
Ouail-01 <ouaillaarabi@gmail.com>
2023-07-05 07:12:51 +0100

    Create a game class

--- classes/games.rb
+++ classes/games.rb
@@ -1,1 +1,12 @@
-# games
+require 'item'
+require 'date'
+
+class Game < Item
+    attr_accessor :multiplayer, :last_played_at
+
+    def initialize(publish_date, multiplayer, last_played_at)
+        super(publish_date)
+        @multiplayer = multiplayer
+        @last_played_at = last_played_at
+    end
+end
