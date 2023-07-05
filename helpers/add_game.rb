2b27c77058e9ad16552e7699a89903664f99099d
Ouail-01 <ouaillaarabi@gmail.com>
2023-07-05 18:28:19 +0100

    Add a game option

--- helpers/add_game.rb
+++ helpers/add_game.rb
@@ -1,11 +1,33 @@
 require_relative '../classes/games'
 require_relative '../classes/author'
+require_relative '../menu'
+
+@games = []
+@authors = []
+@menu = Menu.new
 
 def add_game
+
+  # get the values for author
   puts 'First name of the game author:'
   first_name = gets.chomp
 
   puts 'Last name of the game author:'
   last_name = gets.chomp
 
+  # get the values for game
+  puts 'Publish date of the game:'
+  publish_date = gets.chomp
+
+  puts 'Is it a multiplayer game? [Y/N]'
+  multiplayer = gets.chomp.upcase
+  case multiplayer
+  when 'Y'
+    multiplayer = true
+  when 'N'
+    multiplayer = false
+  end
+
+  puts 'What is the last played date? [YYYY-MM-DD]'
+  last_played_at = DateTime.parse(gets.chomp).to_date
 end
