db51f302c83afcd9ad8ab827e777aee0c1d13796
Ouail-01 <ouaillaarabi@gmail.com>
2023-07-05 16:33:50 +0100

    Add can be archived method

--- classes/games.rb
+++ classes/games.rb
@@ -9,4 +9,9 @@ class Game < Item
     @multiplayer = multiplayer
     @last_played_at = last_played_at
   end
+
+  def can_be_archived?
+    last_played_date = DateTime.parse(@last_played_at).to_date
+    archived = (Date.today.year - last_played_date) > 2
+    super && archived
 end
