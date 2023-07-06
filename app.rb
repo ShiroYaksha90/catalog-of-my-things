77b994dc8ae219ffb7c186f2a2d60ead529f8696
Basim Aljazaeri <30289772+ShiroYaksha90@users.noreply.github.com>
2023-07-06 12:19:28 +0300

    Update app.rb file

--- app.rb
+++ app.rb
@@ -1,23 +1,28 @@
 require_relative 'menu'
 require_relative 'helpers/manage_books'
+require_relative 'helpers/manage_albums'
 
 class App
-  attr_accessor :books, :labels
+  attr_accessor :books, :labels, :albums, :genres
 
   def initialize
     @books = []
     @labels = []
+    @albums = []
+    @genres = []
     @menu = Menu.new
   end
 
   def load_data(app)
     puts '---> Loading data ... <---'
     load_books(app)
+    load_albums(app)
   end
 
   def save_exit(app)
     puts '---> Saving states <---'
     save_book(app)
+    save_album(app)
   end
 
   def run(app)
@@ -27,6 +32,10 @@ class App
       puts '------------------------------'
       option = get_int('Choose: ')
       puts '------------------------------'
+      if option.zero?
+        p 'Quit....!!'
+        exit
+      end
       @menu.manage_options(option, app)
       puts ''
       puts '------------------------------'
